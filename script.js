df74b986859010cf69e13f0ed323b9dcb26468e3
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-08-23 15:37:16 -0400

    create card divs with random unsplash images

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const createGrid = (gridCells) => {
-  grid.style.gridTemplateRows = `repeat(${gridCells}, 1fr)`;
-  grid.style.gridTemplateColumns = `repeat(${gridCells}, 1fr)`;
+const CARD_QTY = 30;
+const grid = document.querySelector('.grid-container');
 
-  for (let r = 0; r < gridCells; r++) {
-    for (let c = 0; c < gridCells; c++) {
-      const child = document.createElement('div');
-      child.addEventListener('mouseover', (e) => {
-        e.target.classList.add('fill');
-      });
-      grid.appendChild(child);
-    }
+const createCards = (qty) => {
+  for (let i = 0; i < qty; i++) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const thumbnail = document.createElement('div');
+    thumbnail.classList.add('thumbnail');
+    thumbnail.style.backgroundImage = `url("https://source.unsplash.com/featured/300x300/?model&${i}")`;
+
+    const labelOne = document.createElement('p');
+    labelOne.classList.add('label-1');
+    labelOne.textContent = 'Song Title';
+
+    const labelTwo = document.createElement('p');
+    labelTwo.classList.add('label-2');
+    labelTwo.textContent = 'Artist Name';
+
+    card.appendChild(thumbnail);
+    card.appendChild(labelOne);
+    card.appendChild(labelTwo);
+
+    grid.appendChild(card);
   }
 };
 
-createGrid(gridCells);
+createCards(CARD_QTY);
